d72087ac7b15bfc1e059930550ca5ef8a2b70b9b
ElkCl0ner <33472254+ElkCl0ner@users.noreply.github.com>
2024-01-03 22:27:57 -0500

    update lobby format

--- Frontend/static/js/lobby.js
+++ Frontend/static/js/lobby.js
@@ -6,12 +6,14 @@ $(document).ready(function() {
         let sid = socket.id;
         // Display lobby code
 
-        // Display enable settings for host
-
         // Display players
 
-        // Display start game button
-        
+        // Display kick player option for host
+
+        // Display enable settings for host
+
+        // Display start game button for host
+
     });
 
     // Connect start_game button
@@ -21,6 +23,10 @@ $(document).ready(function() {
             'turn_time': $('#turn_time').val()
         };
         socket.emit('start_game', data);
+    });
+
+    socket.on('start_game', function(data) {
+
     });
 
 });
