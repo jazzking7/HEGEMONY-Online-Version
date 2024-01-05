09516bb73b9e4fd77192a80eabb3f0fc1e0da0df
ElkCl0ner <33472254+ElkCl0ner@users.noreply.github.com>
2024-01-04 21:27:35 -0500

    display lobby info on lobby page

--- Frontend/static/js/lobby.js
+++ Frontend/static/js/lobby.js
@@ -4,15 +4,31 @@ $(document).ready(function() {
     socket.emit('get_lobby_data');
     socket.on('lobby_data', function(data) {
         let sid = socket.id;
+
         // Display lobby code
+        $("#lobby_code").html(data.lobby_id);
 
         // Display players
+        let players = data.players;
+        let player_list = $("#player_list");
+        for (let i = 0; i < players.length; i++) {
+            let player = players[i];
+            let player_html = '<li class="list-group-item">' + player + '</li>';
+            player_list.append(player_html);
+        }
 
-        // Display kick player option for host
+        // Thats it for non-host players
+        if (sid != data.host) return;
 
-        // Display enable settings for host
+        // Host only options //
+        // TODO Display kick player option
 
-        // Display start game button for host
+        // Display enable settings
+        $("#alliance").prop("disabled", false);
+        $("#turn_time").prop("disabled", false);
+
+        // Display start game button
+        $(".container").append('<div class="row"><button class="btn btn-primary" id="start_game" style="padding: 1rem 5rem;">Start Game</button></div>');
 
     });
 
