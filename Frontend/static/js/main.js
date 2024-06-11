9815fe1aad516f95ed0ce3262f5a28574c24b49b
Jasper Wang <jasperwang50@gmail.com>
2024-06-11 17:13:42 -0400

    deploy aws

--- Frontend/static/js/main.js
+++ Frontend/static/js/main.js
@@ -47,7 +47,7 @@ function unloadScript(script_id) {
 // Initiate socket connection
 // const URL_BACKEND = 'http://127.0.0.1:8081';
 // const URL_BACKEND = 'https://stingray-app-cr93a.ondigitalocean.app/'
-const URL_BACKEND = 'https://3.99.177.92:8081';
+const URL_BACKEND = 'http://3.99.177.92:8081';
 var socket = io.connect(URL_BACKEND);
 
 // Main logic
