8bf067ba41ad1785f77a916f270c3c1ec5503682
Jasper Wang <jasperwang50@gmail.com>
2024-06-19 22:07:45 -0400

    readjust

--- Backend/set_up_scheduler.py
+++ Backend/set_up_scheduler.py
@@ -53,7 +53,7 @@ class setup_event_scheduler:
         for player in gs.players:
             gs.server.emit('choose_color', {'msg': 'Choose a color to represent your country', 'options': color_options}, room=player)
         
-        ms.selection_time_out(10, len(gs.players))
+        ms.selection_time_out(20, len(gs.players))
 
         # handle timeout
         for player in gs.players.values():
@@ -103,7 +103,7 @@ class setup_event_scheduler:
             gs.server.emit('choose_territorial_distribution', {'options': gs.aval_choices}, room=player)
             
 
-            ms.selection_time_out(10, 1)
+            ms.selection_time_out(20, 1)
             
             # handle timeout
             if not ms.selected:
@@ -122,7 +122,7 @@ class setup_event_scheduler:
         gs.server.emit('set_up_announcement', {'msg':f"Settle your capital!"}, room=gs.lobby)
         gs.server.emit('change_click_event', {'event': "settle_capital"}, room=gs.lobby)
 
-        ms.selection_time_out(10, len(gs.players))
+        ms.selection_time_out(20, len(gs.players))
 
         # handle not choosing
         for player in gs.players.values():
@@ -140,7 +140,7 @@ class setup_event_scheduler:
         gs.server.emit('set_up_announcement', {'msg':f"Build up two cities!"}, room=gs.lobby)
         gs.server.emit('change_click_event', {'event': "settle_cities"}, room=gs.lobby)
 
-        ms.selection_time_out(10, len(gs.players))
+        ms.selection_time_out(20, len(gs.players))
 
 
         for player in gs.players.values():
@@ -166,7 +166,7 @@ class setup_event_scheduler:
             gs.players[player].deployable_amt = amount
             gs.server.emit('troop_deployment', {'amount': amount}, room=player)
 
-        ms.selection_time_out(10, len(gs.players))
+        ms.selection_time_out(30, len(gs.players))
 
         gs.signal_view_clear()
         gs.server.emit('change_click_event', {'event': None}, room=gs.lobby)
