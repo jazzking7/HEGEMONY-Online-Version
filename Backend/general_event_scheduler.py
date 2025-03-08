710c52479baf668b9ac147ccc67c13bdddab4479
Jasper Wang <jasperwang50@gmail.com>
2025-03-08 00:17:22 -0500

    fix lag

--- Backend/general_event_scheduler.py
+++ Backend/general_event_scheduler.py
@@ -85,7 +85,7 @@ class General_Event_Scheduler:
         return
 
     def run_setup_events(self,):
-        time.sleep(6)
+        time.sleep(15)
         self.gs.server.emit('set_up_announcement', {'msg': "Loading resources..."}, room=self.gs.lobby)
         time.sleep(2)
         for event in self.SES:
