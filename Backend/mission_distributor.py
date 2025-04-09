be340da05c66261cbe96d460e2e4e6f84bb19617
Jasper Wang <jasperwang50@gmail.com>
2025-04-09 02:03:56 -0400

    back to 20x20

--- Backend/mission_distributor.py
+++ Backend/mission_distributor.py
@@ -67,18 +67,19 @@ class Mission_Distributor:
         self.dup_con = ['Pop', 'Exp', 'Ind', 'Dom']
         self.self_wins = ['Loy', 'Bon', 'Dec', 'War', 'Pac', 'Str', 'Due', 'Pun', 'Sur', 'Ass', 'Pro', 'Gam']
         self.missions = [
-            #'Pac', 
-            'War', 'Loy', 'Bon',
-            #'Uni', 'Pol', 'Fan', 
-            'Ind',
-            'Exp', 'Pop',
-            #'Dom',
-            'Gua',
-            'Dec',
-            #'Str', 'Due', 'Pun',
-            #'Sur',
-            #'Ass', 'Pro',
-            #'Gam'
+             'Pac', 
+             'War', 'Loy', 'Bon',
+             'Uni', 'Pol', 'Fan', 
+             'Ind',
+             'Exp', 'Pop',
+             'Dom',
+             'Gua',
+             'Dec',
+             'Str', 
+             'Due', 'Pun',
+             'Sur',
+             'Ass', 'Pro',
+             'Gam'
         ]
 
         self.S_tier = ['Decapitator', 'Pacifist', 'Starchaser', 'Duelist', 'Punisher', 'Assassin', 'Protectionist']
