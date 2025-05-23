21c16c380995794cf1715eefb21e8ada4c2d6d91
Jasper Wang <jasperwang50@gmail.com>
2025-05-22 23:59:31 -0400

    Missions

--- Backend/missions.py
+++ Backend/missions.py
@@ -254,7 +254,7 @@ class Bounty_Hunter(Mission):
         nump = len(gs.pids)
         if nump < 5:
             numt = 1
-        elif nump < 9:
+        elif nump < 8:
             numt = 2
         elif nump < 16:
             numt = 3
@@ -780,9 +780,9 @@ class Starchaser(Mission):
         
         self.chase_completed = 0
         self.target_chases = 7
-        if m > 80:
+        if m > 85:
             self.target_chases += 3
-        elif m > 60:
+        elif m > 65:
             self.target_chases += 2
         elif m > 40:
             self.target_chases += 1
