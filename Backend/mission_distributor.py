7ba7480be2da3a175b1b454446c61bfb1e036591
Jasper Wang <jasperwang50@gmail.com>
2026-02-22 20:06:55 -0500

    mission pre-set

--- Backend/mission_distributor.py
+++ Backend/mission_distributor.py
@@ -135,6 +135,8 @@ class Mission_Distributor:
 
     # Generate mission set based on number of players
     def get_mission_set(self, num_p, complexity="pioneer"):
+        if num_p == 5:
+            return ['Sur', 'Exp', 'Ind', 'Dom', 'Pun']
         mission_pool = self.missions
         if complexity == 'beginner':
             mission_pool = self.beginner
